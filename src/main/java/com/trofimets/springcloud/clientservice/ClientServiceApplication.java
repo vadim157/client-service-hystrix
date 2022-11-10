b3eb50cdeab465845850b31c86b9e46590de1982
vadim157 <trofimets@inbox.ru>
2022-11-10 21:37:33 +0300

    commit

--- src/main/java/com/trofimets/springcloud/clientservice/ClientServiceApplication.java
+++ src/main/java/com/trofimets/springcloud/clientservice/ClientServiceApplication.java
@@ -2,7 +2,10 @@ package com.trofimets.springcloud.clientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +13,9 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@EnableHystrix
 public class ClientServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/trofimets/springcloud/clientservice/service/ClientService.java
+++ src/main/java/com/trofimets/springcloud/clientservice/service/ClientService.java
@@ -1,5 +1,6 @@
 package com.trofimets.springcloud.clientservice.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.trofimets.springcloud.clientservice.connector.BookServiceConnector;
 import com.trofimets.springcloud.clientservice.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,14 +8,22 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Service
+@Transactional(readOnly = true)
 public class ClientService {
 
     private BookServiceConnector connector;
     private RestTemplate restTemplate;
+    private final Logger LOG = Logger.getLogger(ClientService.class.getName());
 
     @Autowired
     public ClientService(BookServiceConnector connector, RestTemplate restTemplate) {
@@ -22,15 +31,24 @@ public class ClientService {
         this.restTemplate = restTemplate;
     }
 
+    @HystrixCommand(fallbackMethod = "failed")
     public List<Book> getAllBooks() {
         return connector.getAllBooks();
     }
-    public List<Book> getAllBooksRT(){
+
+    public List<Book> getAllBooksRT() {
         ResponseEntity<List<Book>> responseEntity = restTemplate.exchange("http://localhost:8081/data",
                 HttpMethod.GET, null,
                 new ParameterizedTypeReference<List<Book>>() {
                 });
         List<Book> books = responseEntity.getBody();
+        return books;
+    }
+
+    public List<Book> failed() {
+        String error = "Service is not available now. Please try again later";
+        LOG.log(Level.INFO, error);
+        List<Book> books = new ArrayList<>(Arrays.asList(new Book("null","null","null","null")));
         return books;
     }
 }
